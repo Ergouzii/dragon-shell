9b52d3ef27da70ed1f99cf68c17ddc089878b202
Xiaohui Liu <xiaohui1@cs.ualberta.ca>
2019-09-17 14:54:59 -0600

    very early version, nothing working

--- A1/dragonshell.c
+++ A1/dragonshell.c
@@ -1,6 +1,8 @@
 #include <stddef.h>
 #include <string.h>
 
+#include <readline/readline.h> 
+
 #include <stdio.h>
 
 /**
@@ -20,10 +22,39 @@ void tokenize(char* str, const char* delim, char ** argv) {
   }
 }
 
+void welcome(void) {
+  printf("**********************************************");
+  printf("**********Welcome to the Dragonshell**********");
+  printf("**********************************************");
+}
+
+void cmdLoop(void) {
+  int running;
+  do {
+    readInput();
+  } while(running);
+}
+
+int readInput() {
+  char* input;
+
+  input = readline("\ndragonshell>> "); // get input
+
+  if (strlen(input) != 0) { // if input is not empty
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
 int main(int argc, char **argv) {
   // print the string prompt without a newline, before beginning to read
   // tokenize the input, run the command(s), and print the result
   // do this in a loop
+
+  welcome();
+
+  cmdLoop();
 
   return 0;
 }
