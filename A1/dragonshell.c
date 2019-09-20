a45379aa18282ca8d0cc5cbdc7b365acf4ffbafa
Xiaohui Liu <xiaohui1@cs.ualberta.ca>
2019-09-19 22:29:25 -0600

    exit cmd done

--- A1/dragonshell.c
+++ A1/dragonshell.c
@@ -35,8 +35,10 @@ const char *BAD = "**BAD**";
 
 void tokenize(char *str, const char *delim, char **argv);
 int handle_input();
-int handle_cd();
-int handle_pwd();
+int handle_cd(char *tokenized[]);
+int handle_pwd(char *tokenized[]);
+int handle_path(char *tokenized[]);
+int handle_exit(char *tokenized[]);
 
 int main(int argc, char **argv) {
   // print the string prompt without a newline, before beginning to read
@@ -75,20 +77,26 @@ int handle_input() {
   char input[100];
   fgets(input, sizeof(input), stdin); // get input
 
+  // if (input[0] == ' ') {
+  //   char *space_input = "dragonshell: please do not include space at start\n";
+  //   printf("%s", space_input);
+  //   exit(1);
+  // }
+
   // remove newline at the end of "fgets"
   size_t len = strlen(input) - 1;
   if (input[len] == '\n') {
     input[len] = '\0';
   }
 
-  // if input is empty
   size_t empty = 0;
-  if (strlen(input) == empty) { 
+  if (strlen(input) == empty) { // if input is empty
     char *empty_input = "dragonshell: please give an input\n";
-    printf("%s", empty_input);
-    
+    printf("%s", empty_input);  
+  } else if (input[0] == ' ') { // if space at start of line
+    char *space_start = "dragonshell: please do not include space at start of line\n";
+    printf("%s", space_start);  
   } else { // if input is not empty
-
     // tokenize the input
     char *delim = " ";
     char **tokenized = malloc(sizeof(char) * 100);
@@ -100,11 +108,17 @@ int handle_input() {
     // check which cmd is entered and handle them
     char *cd_cmd = "cd";
     char *pwd_cmd = "pwd";
+    char *exit_cmd = "exit";
     char *input_cmd = tokenized[0];
     if (strcmp(input_cmd, cd_cmd) == 0) { // if cd cmd
       handle_cd(tokenized);
     } else if (strcmp(input_cmd, pwd_cmd) == 0) { // if pwd cmd
       handle_pwd(tokenized);
+    } else if (strcmp(input_cmd, exit_cmd) == 0) {
+      handle_exit(tokenized);
+    } else {
+      char *unknown = "dragonshell: unknown command\n";
+      printf("%s", unknown);
     }
   }
 
@@ -139,5 +153,16 @@ int handle_cd(char *tokenized[]) {
 int handle_pwd(char *tokenized[]) {
   char cwd[100];
   printf("%s\n", getcwd(cwd, 100)); 
+  return 0;
+}
+
+int handle_path(char *tokenized[]) {
+  return 0;
+}
+
+int handle_exit(char *tokenized[]) {
+  char *exiting = "dragonshell: Exiting\n";
+  printf("%s\n", exiting);
+  _exit(1);
   return 0;
 }
