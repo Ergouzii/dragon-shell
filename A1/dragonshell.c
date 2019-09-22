e9f0ed19eaa789639bb91e5dee77980680026b00
Xiaohui Liu <xiaohui1@cs.ualberta.ca>
2019-09-21 20:37:53 -0600

    basic_commands done (bugs may exist)

--- A1/dragonshell.c
+++ A1/dragonshell.c
@@ -29,16 +29,18 @@ const char *WELCOME = "...........................................\n\
 ..........*WWWWWWWWWWWWWWWWWWWWW-..........\n\n\
 ********Welcome to the Dragonshell*********\n";
 
+char PATH[100] = "/bin/:/usr/bin/";
+
 // TODO: these are for debugging uses
 const char *OK = "**OK**";
 const char *BAD = "**BAD**";
 
 void tokenize(char *str, const char *delim, char **argv);
-int handle_input(char *path);
+int handle_input();
 int handle_cd(char *tokenized[]);
 int handle_pwd(char *tokenized[]);
-int handle_path(char *tokenized[], char *path);
-int handle_a2path(char *tokenized[], char *path);
+int handle_path(char *tokenized[]);
+int handle_a2path(char *tokenized[]);
 int handle_exit(char *tokenized[]);
 
 int main(int argc, char **argv) {
@@ -48,10 +50,8 @@ int main(int argc, char **argv) {
 
   printf("%s", WELCOME); // prints welcome text
 
-  char *init_path = "/bin/:/usr/bin/";
-
   while (1) {
-    handle_input(init_path);
+    handle_input();
   }
 
   return 0;
@@ -74,11 +74,12 @@ void tokenize(char *str, const char *delim, char **argv) {
   }
 }
 
-int handle_input(char *path) {
-
+int handle_input() {
   printf("\ndragonshell>> ");
+
+  // get input
   char input[100];
-  if (fgets(input, sizeof(input), stdin) == NULL) { // get input
+  if (fgets(input, sizeof(input), stdin) == NULL) { 
     // catching ctrl + D, exit shell as catched
     char *exiting = "\ndragonshell: Exiting\n";
     printf("%s\n", exiting);
@@ -118,9 +119,9 @@ int handle_input(char *path) {
     } else if (strcmp(input_cmd, exit_cmd) == 0) {
       handle_exit(tokenized);
     } else if (strcmp(input_cmd, path_cmd) == 0) {
-      handle_path(tokenized, path);
+      handle_path(tokenized);
     } else if (strcmp(input_cmd, a2path_cmd) == 0) {
-      handle_a2path(tokenized, path);
+      handle_a2path(tokenized);
     } else {
       char *unknown = "dragonshell: Command not found\n";
       printf("%s", unknown);
@@ -149,6 +150,7 @@ int handle_cd(char *tokenized[]) {
       } else {
         char *wrong_dir = "dragonshell: No such file or directory\n";
         printf("%s", wrong_dir);
+        return 1;
       }
     }
   }
@@ -161,18 +163,45 @@ int handle_pwd(char *tokenized[]) {
   return 0;
 }
 
-int handle_path(char *tokenized[], char *path) {
-  printf("Current PATH: %s\n", path);
+int handle_path(char *tokenized[]) {
+  printf("Current PATH: %s\n", PATH);
   return 0;
 }
 
-int handle_a2path(char *tokenized[], char *path) {
-
+int handle_a2path(char *tokenized[]) {
+  char *arg = tokenized[1];
+  if (arg == NULL) { // if no argument given
+    char *no_argument = "dragonshell: expected argument to \"a2path\"\n";
+    printf("%s", no_argument);
+    return 1;
+  } else if (tokenized[2] != NULL) { // if 3rd argument is given
+    char *three_arguments = "dragonshell: too many arguments for \"a2path\"\n";
+    printf("%s", three_arguments);
+    return 1;
+  } else { // if only 1 arg given (normal use case)
+    if (strncmp(arg, "$PATH:", 6) != 0) { // if arg does not start with "$PATH"
+      char *no_append_path = "dragonshell: please enter in form of \"a2path $PATH:/*/*/*\"\n";
+      printf("%s", no_append_path);
+      return 1;
+    } else { // if arg gives something to add
+      char **tokenized_arg = malloc(sizeof(char) * 100);
+      char *delim = ":";
+      tokenize(arg, delim, tokenized_arg); // split the paths by ":"
+      if (strcmp(tokenized_arg[0], "$PATH") != 0) { // if not adding path to $PATH
+        char *wrong_append = "dragonshell: please add path to $PATH\n";
+        printf("%s", wrong_append);
+        return 1;
+      } else { // if adding to $PATH
+        strcat(PATH, ":");
+        strcat(PATH, tokenized_arg[1]);
+      }
+      free(tokenized_arg);
+    }
+  }
   return 0;
 }
 
 int handle_exit(char *tokenized[]) {
-  // TODO: add keydown events
   char *exiting = "dragonshell: Exiting\n";
   printf("%s\n", exiting);
   _exit(1);
